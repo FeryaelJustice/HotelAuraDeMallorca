de5a32f440f9b1175d5b60fe20b4768a911465c7
Fernando Gonzalez <fernando.gonzalez@homerti.com>
2023-10-10 16:23:29 +0200

    db en gestor cms traducciones corregido

--- cms/database/db.sql
+++ cms/database/db.sql
@@ -13,42 +13,42 @@ CREATE TABLE app_user (
     user_verified BOOLEAN
 );
 
--- Create the table page
-CREATE TABLE page (
+-- Create the table app_page
+CREATE TABLE app_page (
     id INT PRIMARY KEY,
-    page_name VARCHAR(30)
+    app_page_name VARCHAR(30)
 );
 
--- Create the table language
-CREATE TABLE language (
+-- Create the table lang
+CREATE TABLE lang (
     id INT PRIMARY KEY,
-    language_code VARCHAR(6),
-    language_name VARCHAR(30),
+    lang_code VARCHAR(6),
+    lang_name VARCHAR(30)
 );
 
--- Create the table page_language
-CREATE TABLE page_language (
-    page_id INT,
-    language_id,
-    PRIMARY KEY (page_id, language_id),
-    FOREIGN KEY (page_id) REFERENCES page(id),
-    FOREIGN KEY (language_id) REFERENCES language(id)
+-- Create the table page_lang
+CREATE TABLE app_page_lang (
+    id INT PRIMARY KEY,
+    app_page_id INT,
+    lang_id INT,
+    FOREIGN KEY (app_page_id) REFERENCES app_page(id),
+    FOREIGN KEY (lang_id) REFERENCES lang(id)
 );
 
 -- Create the table section
 CREATE TABLE section (
     id INT PRIMARY KEY,
-    page_id INT,
+    app_page_id INT,
     section_name VARCHAR(30),
     section_parent INT,
-    FOREIGN KEY (page_id) REFERENCES page(id),
+    FOREIGN KEY (app_page_id) REFERENCES app_page(id),
     FOREIGN KEY (section_parent) REFERENCES section(id)
 );
 
 -- Create the table literal
 CREATE TABLE literal (
     id INT PRIMARY KEY,
-    literal_text TEXT,
+    literal_text TEXT
 );
 
 -- Create the table section_literal (Intermediate Table)
@@ -80,35 +80,88 @@ VALUES
         false
     );
 
--- Servicios asociados a reservas
+-- INSERTS en la tabla app_page
 INSERT INTO
-    booking_service (booking_id, service_id)
+    app_page (id, app_page_name)
+VALUES
+    (1, 'Homerti'),
+    (2, 'Vacalia'),
+    (3, 'Aura de Mallorca');
+
+-- INSERTS en la tabla lang
+INSERT INTO
+    lang (id, lang_code, lang_name)
+VALUES
+    (1, 'en', 'English'),
+    (2, 'es', 'Spanish'),
+    (3, 'ca', 'Catalan'),
+    (4, 'de', 'German');
+
+-- INSERTS en la tabla page_lang
+INSERT INTO
+    app_page_lang (id, app_page_id, lang_id)
+VALUES
+    (1, 1, 1),
+    -- Homerti - English
+    (2, 1, 2),
+    -- Homerti - Spanish
+    (3, 1, 3),
+    -- Homerti - Catalan
+    (4, 2, 1),
+    -- Vacalia - English
+    (5, 2, 2),
+    -- Vacalia - Spanish
+    (6, 2, 1),
+    -- Aura de Mallorca - Spanish
+    (7, 3, 2),
+    -- Aura de Mallorca - English
+    (8, 3, 1);
+
+-- INSERTS en la tabla section
+INSERT INTO
+    section (id, app_page_id, section_name, section_parent)
+VALUES
+    (1, 1, 'Title', NULL),
+    (2, 1, 'Header', NULL),
+    (3, 1, 'Nav', 2),
+    (4, 2, 'Title', NULL),
+    (5, 2, 'Header', NULL),
+    (6, 2, 'Nav', 5),
+    (7, 3, 'Title', NULL),
+    (8, 3, 'Header', NULL),
+    (9, 3, 'Nav', 8),
+    (10, 3, 'Contact Section', NULL);
+
+-- INSERTS en la tabla literal
+INSERT INTO
+    literal (id, literal_text)
+VALUES
+    (1, 'Welcome to our website.'),
+    (2, 'Learn more about us.'),
+    (3, 'Contact us for inquiries.');
+
+-- INSERTS en la tabla section_literal
+INSERT INTO
+    section_literal (section_id, literal_id)
 VALUES
     (1, 1),
-    (1, 2),
-    (2, 3);
+    -- Title - Welcome
+    (2, 2),
+    -- Header - Learn more
+    (3, 3),
+    -- Nav - Contact us
+    (4, 1),
+    -- Title - Welcome
+    (5, 2),
+    -- Header - Welcome
+    (6, 3),
+    -- Nav - Welcome
+    (7, 1),
+    -- Title - Welcome
+    (8, 2),
+    -- Header - Welcome
+    (9, 3),
+    -- Nav - Welcome
+    (10, 3);
 
--- Condiciones meteorológicas
-INSERT INTO
-    weather (
-        id,
-        weather_date,
-        weather_state,
-        affected_service_id
-    )
-VALUES
-    (1, '2023-10-22', 'Sunny', 1),
-    (2, '2023-11-05', 'Rainy', 3);
-
--- Payment (transacciones)
-INSERT INTO
-    payment (
-        id,
-        user_id,
-        booking_id,
-        payment_amount,
-        payment_date
-    )
-VALUES
-    (1, 1, 1, 50.00, '2023-10-23'),
-    (2, 2, 2, 150.00, '2023-11-10');
+-- Contact Section - Contact us
