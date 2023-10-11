a04a1d2a7b44374cf41cd0b9245f8f950e81acdb
Fernando Gonzalez <fernando.gonzalez@homerti.com>
2023-10-11 15:44:51 +0200

    primer parallax home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,18 @@
+import './Home.css';
+import { Parallax } from "react-parallax";
+import Image from './../assets/images/pexels-maria-orlova-garden.webp'
+
 export function Home() {
     // Get env variables nativa de vite pero es SOLO para react
     console.log(import.meta.env)
     return (
         <div>
-            Home
+            <Parallax strength={600} bgImage={Image}>
+                <div className="content" id='home'>
+                    <div className="text-content">
+                        Welcome to Aura de Mallorca                    </div>
+                </div>
+            </Parallax>
             <br />
             Server is running in {import.meta.env.MODE}
         </div>
