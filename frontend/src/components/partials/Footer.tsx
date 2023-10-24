64a1c70103dcc2b5b34124de2f92f44b24ce4eaa
Fernando Gonzalez <fernando.gonzalez@homerti.com>
2023-10-24 13:06:35 +0200

    env timezone and app names, contact form fix and footer DONE

--- frontend/src/components/partials/Footer.tsx
+++ frontend/src/components/partials/Footer.tsx
@@ -1,8 +1,21 @@
 export const Footer = () => {
     return (
         <footer id="footer" className="footer">
-            Footer:
-            Server is running in {import.meta.env.MODE}
+            <div className="footer-sides">
+                <div className="footer-left">
+                    <strong>{process.env.APP_NAME}</strong>
+                    <p>Enjoy Mallorca in its purest form from our cozy hotel located in a spectacular area of Mallorca, historical and artistic heritage.
+                        A cozy and romantic hotel to enjoy this unique enclave in a simple and elegant environment.</p>
+                </div>
+                <div className="footer-right">
+                    <span>Tlf: <a href="tel:123456789">Call us</a></span>
+                    <span>Whatsapp: <a href="https://api.whatsapp.com/send?phone=123456789">Send us a message</a> </span>
+                    <span>Email: <a href="mailto:hotelaurademallorca@aurademallorca.com">Contact us</a></span>
+                </div>
+            </div>
+            <div>
+                <em>Page is running in {import.meta.env.MODE}</em>
+            </div>
         </footer>
     )
 }
--- frontend/src/pages/Contact.tsx
+++ frontend/src/pages/Contact.tsx
@@ -32,22 +32,22 @@ export const Contact = () => {
         <div className='contactPage'>
             <h1>Contact us!</h1>
             <Form className='contactForm' onSubmit={handleSubmit}>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Group className="mb-3" controlId="email">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" id='email' name='email' placeholder="Enter email" className='input' onChange={(event) => setEmail(event.target.value)} />
+                    <Form.Control type="email" name='email' placeholder="Enter email" className='input' onChange={(event) => setEmail(event.target.value)} />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicSubject">
+                <Form.Group className="mb-3" controlId="subject">
                     <Form.Label>Subject</Form.Label>
-                    <Form.Control type="text" id='subject' name='subject' maxLength={200} placeholder="Subject" className='input' onChange={(event) => setSubject(event.target.value)} />
+                    <Form.Control type="text" name='subject' maxLength={200} placeholder="Subject" className='input' onChange={(event) => setSubject(event.target.value)} />
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicMessage">
+                <Form.Group className="mb-3" controlId="message">
                     <Form.Label>Message</Form.Label>
-                    <Form.Control type="text" id='message' name='message' maxLength={2000} placeholder="Message" className='input' onChange={(event) => setMessage(event.target.value)} />
+                    <Form.Control as='textarea' rows={8} name='message' maxLength={1000} placeholder="Message" className='input' onChange={(event) => setMessage(event.target.value)} />
                 </Form.Group>
 
                 <Button variant="primary" type="submit">
