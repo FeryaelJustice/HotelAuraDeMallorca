cac57fb60a067fc01770176aff5c86d4a92235ed
Fernando Gonzalez <fernando.gonzalez@homerti.com>
2023-11-02 16:54:43 +0100

    translations data url

--- frontend/src/lang/i18n.ts
+++ frontend/src/lang/i18n.ts
@@ -6,10 +6,11 @@ import { LANGUAGES } from "../constants";
 
 const languageCodes: string[] = LANGUAGES.map((language) => language.code);
 
-const getCurrentHost =
-  import.meta.env.MODE === "development"
+const getCurrentHost = process.env.TRANSLATIONS_DATA_URL;
+/*import.meta.env.MODE === "development"
     ? process.env.DEV_FRONT_URL
     : process.env.FRONT_URL;
+  */
 
 i18n
   .use(LanguageDetector)
