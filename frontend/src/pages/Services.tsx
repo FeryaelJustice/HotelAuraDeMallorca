04fa1b9a083fffac28264276ff35530bc64daf62
Fernando Gonzalez <fernando.gonzalez@homerti.com>
2023-11-14 16:48:53 +0100

    fix styles of services page

--- frontend/src/pages/Services.tsx
+++ frontend/src/pages/Services.tsx
@@ -63,11 +63,11 @@ export const Services = ({ colorScheme, openImagePreviewModal }: ServicesProps) 
                             <div>
                                 {services.map((service) => (
                                     <Row key={service.id ? (service.id + Math.random() * (1000 - 1)) : Math.random()} md={12} className="mb-12">
-                                        <Card style={{ cursor: 'pointer', backgroundImage: `url(${service.imageURL})`, backgroundSize: 'cover', backgroundPositionY: 'center', borderRadius: '12px', marginBottom: '8px', transition: 'transform 0.6s', display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+                                        <Card style={{height: '360px', cursor: 'pointer', backgroundImage: `url(${service.imageURL})`, backgroundSize: 'cover', backgroundPositionY: 'center', borderRadius: '12px', marginBottom: '8px', transition: 'transform 0.6s', display: 'flex', justifyContent: 'center', alignItems: 'center' }}
                                             onMouseEnter={(e) => { e.currentTarget.style.transform = 'scale(1.04)' }} onMouseLeave={(e) => { e.currentTarget.style.transform = 'scale(1)'; }}
                                             onClick={() => openImagePreviewModal(service.imageURL ? service.imageURL : '', service.name ? service.name : '')}
                                         >
-                                            <Card.Body style={{ textShadow: colorScheme !== "light" ? '2px 2px black' : '1px 1px 1px white', color: colorScheme == "light" ? 'black' : 'white', background: colorScheme == "dark" ? ' rgba(0, 0, 0, .9)' : 'rgba(255, 255, 255, .9)', width: 'fit-content', borderRadius: '8px' }}>
+                                            <Card.Body style={{ display:'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', textAlign: 'center', textShadow: colorScheme !== "light" ? '2px 2px black' : '1px 1px 1px white', color: colorScheme == "light" ? 'black' : 'white', background: colorScheme == "dark" ? ' rgba(0, 0, 0, .9)' : 'rgba(255, 255, 255, .9)', borderRadius: '8px' }}>
                                                 <Card.Title>{service.name}</Card.Title>
                                                 <Card.Text>
                                                     <span>{service.description}</span>
