b115748bc61ba7ae3edfecd1061afb33059bddc1
FeryaelJustice <nano9gs@hotmail.es>
2025-09-10 20:06:40 +0200

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { CookiesProvider } from 'react-cookie';
 import './index.css'
 import "./lang/i18n";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <App />
+  <CookiesProvider defaultSetOptions={{ path: '/' }}>
+    <App />
+  </CookiesProvider>
 )
